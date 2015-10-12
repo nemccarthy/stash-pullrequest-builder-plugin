80431f422dce9ea763e59c28e35fef75113203b1
Jonathan Strickland <jonatstr@cisco.com>
2015-10-12 15:30:06 -0400

    JENKINS-30558 - Defensive code to prevent native deadlock and cleanup socket code

--- src/main/java/stashpullrequestbuilder/stashpullrequestbuilder/stash/StashApiClient.java
+++ src/main/java/stashpullrequestbuilder/stashpullrequestbuilder/stash/StashApiClient.java
@@ -9,7 +9,9 @@ import org.apache.commons.httpclient.methods.DeleteMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.apache.commons.httpclient.params.HttpParams;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.params.CoreConnectionPNames;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ObjectNode;
 
@@ -20,6 +22,11 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,6 +34,11 @@ import java.util.logging.Logger;
  * Created by Nathan McCarthy
  */
 public class StashApiClient {
+	
+	private static final int HTTP_REQUEST_TIMEOUT_SECONDS = 30;
+	private static final int HTTP_CONNECTION_TIMEOUT_SECONDS = 10;
+	private static final int HTTP_SOCKET_TIMEOUT_SECONDS = 10;
+		
     private static final Logger logger = Logger.getLogger(StashApiClient.class.getName());
     private static final ObjectMapper mapper = new ObjectMapper();
 
@@ -115,6 +127,10 @@ public class StashApiClient {
 
     private HttpClient getHttpClient() {
         HttpClient client = new HttpClient();
+    	HttpParams httpParams = client.getParams();    	
+    	httpParams.setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, StashApiClient.HTTP_CONNECTION_TIMEOUT_SECONDS * 1000);
+    	httpParams.setParameter(CoreConnectionPNames.SO_TIMEOUT, StashApiClient.HTTP_SOCKET_TIMEOUT_SECONDS * 1000);
+        
 //        if (Jenkins.getInstance() != null) {
 //            ProxyConfiguration proxy = Jenkins.getInstance().proxy;
 //            if (proxy != null) {
@@ -138,19 +154,59 @@ public class StashApiClient {
         HttpClient client = getHttpClient();
         client.getState().setCredentials(AuthScope.ANY, credentials);
         GetMethod httpget = new GetMethod(path);
+        httpget.setRequestHeader("Connection", "close");
         client.getParams().setAuthenticationPreemptive(true);
         String response = null;
+        FutureTask<String> httpTask = null;
+        Thread thread;
         try {
-            client.executeMethod(httpget);
-            InputStream responseBodyAsStream = httpget.getResponseBodyAsStream();
-            StringWriter stringWriter = new StringWriter();
-            IOUtils.copy(responseBodyAsStream, stringWriter, "UTF-8");
-            response = stringWriter.toString();
-        } catch (HttpException e) {
+            httpTask = new FutureTask<String>(new Callable<String>() {
+
+            	private HttpClient client;
+            	private GetMethod httpget;
+            	
+                @Override
+                public String call() throws Exception {
+            		String response = null;
+                	try {
+                        client.executeMethod(httpget);
+                        InputStream responseBodyAsStream = httpget.getResponseBodyAsStream();
+                        StringWriter stringWriter = new StringWriter();
+                        IOUtils.copy(responseBodyAsStream, stringWriter, "UTF-8");
+                        response = stringWriter.toString();
+                    } catch (HttpException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                	
+                	return response;
+
+                }
+                
+                public Callable<String> init(HttpClient client, GetMethod httpget) {
+                	this.client = client;
+                	this.httpget = httpget;
+                	return this;
+                }
+                
+              }.init(client, httpget));
+            thread = new Thread(httpTask);
+            thread.start();
+            response = httpTask.get((long)StashApiClient.HTTP_REQUEST_TIMEOUT_SECONDS, TimeUnit.SECONDS);
+            
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        } catch (IOException e) {
+        } catch (ExecutionException e) {
             e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+            httpget.abort();
+            httpTask.cancel(true);
+        } finally {
+        	httpget.releaseConnection();
         }
+            
         logger.log(Level.FINEST, "PR-GET-RESPONSE:" + response);
         return response;
     }
@@ -159,13 +215,54 @@ public class StashApiClient {
         HttpClient client = getHttpClient();
         client.getState().setCredentials(AuthScope.ANY, credentials);
         DeleteMethod httppost = new DeleteMethod(path);
+        httppost.setRequestHeader("Connection", "close");
         client.getParams().setAuthenticationPreemptive(true);
         int res = -1;
+        FutureTask<Integer> httpTask = null;
+        Thread thread;
+
         try {
-            res = client.executeMethod(httppost);
-        } catch (IOException e) {
+            httpTask = new FutureTask<Integer>(new Callable<Integer>() {
+
+            	private HttpClient client;
+            	private DeleteMethod httppost;
+            	
+                @Override
+                public Integer call() throws Exception {
+                	int res = -1;
+                    try {
+                        res = client.executeMethod(httppost);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    
+                    return res;
+
+                }
+                
+                public Callable<Integer> init(HttpClient client, DeleteMethod httppost) {
+                	this.client = client;
+                	this.httppost = httppost;
+                	return this;
+                }
+                
+              }.init(client, httppost));
+            thread = new Thread(httpTask);
+            thread.start();
+            res = httpTask.get((long)StashApiClient.HTTP_REQUEST_TIMEOUT_SECONDS, TimeUnit.SECONDS);
+            
+        } catch (InterruptedException e) {
             e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+            httppost.abort();
+            httpTask.cancel(true);
+        } finally {
+        	httppost.releaseConnection();
         }
+        
         logger.log(Level.FINE, "Delete comment {" + path + "} returned result code; " + res);
     }
 
@@ -174,6 +271,7 @@ public class StashApiClient {
         HttpClient client = getHttpClient();
         client.getState().setCredentials(AuthScope.ANY, credentials);
         PostMethod httppost = new PostMethod(path);
+        httppost.setRequestHeader("Connection", "close");
 
         ObjectNode node = mapper.getNodeFactory().objectNode();
         node.put("text", comment);
@@ -191,20 +289,62 @@ public class StashApiClient {
         httppost.setRequestEntity(requestEntity);
         client.getParams().setAuthenticationPreemptive(true);
         String response = "";
+        FutureTask<String> httpTask = null;
+        Thread thread;
+
         try {
-            client.executeMethod(httppost);
-            InputStream responseBodyAsStream = httppost.getResponseBodyAsStream();
-            StringWriter stringWriter = new StringWriter();
-            IOUtils.copy(responseBodyAsStream, stringWriter, "UTF-8");
-            response = stringWriter.toString();
-            logger.info("API Request Response: " + response);
-        } catch (HttpException e) {
+            httpTask = new FutureTask<String>(new Callable<String>() {
+
+            	private HttpClient client;
+            	private PostMethod httppost;
+            	
+                @Override
+                public String call() throws Exception {
+                	String response = "";
+                	
+                    try {
+                        client.executeMethod(httppost);
+                        InputStream responseBodyAsStream = httppost.getResponseBodyAsStream();
+                        StringWriter stringWriter = new StringWriter();
+                        IOUtils.copy(responseBodyAsStream, stringWriter, "UTF-8");
+                        response = stringWriter.toString();
+                        logger.info("API Request Response: " + response);
+                    } catch (HttpException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    
+                    return response;
+
+                }
+                
+                public Callable<String> init(HttpClient client, PostMethod httppost) {
+                	this.client = client;
+                	this.httppost = httppost;
+                	return this;
+                }
+                
+              }.init(client, httppost));
+            thread = new Thread(httpTask);
+            thread.start();
+            response = httpTask.get((long)StashApiClient.HTTP_REQUEST_TIMEOUT_SECONDS, TimeUnit.SECONDS);
+            
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        } catch (IOException e) {
+        } catch (ExecutionException e) {
             e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+            httppost.abort();
+            httpTask.cancel(true);            
+        } finally {
+        	httppost.releaseConnection();
         }
+                
         logger.log(Level.FINEST, "PR-POST-RESPONSE:" + response);
         return response;
+        
 
     }
 
